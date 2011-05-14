6e6562c25532fcced4f3bb330b6e80ffdc40b935
Enrique Arango Lyons <karlysw@gmail.com>
2011-05-14 09:38:09 -0500

    initial commit

---
+++ TicTacToeApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToeApp::Application.config.secret_token = '785aa7ad3bfc893f3d83f04022ed4c64ed71e2133ff2bae3453dcf955500dd933719353abed876b12e2282bf2b2f15f308532d0272f08d072f4bb1dd247e60ec'
---
+++ TicTacToeWS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToeWS::Application.config.secret_token = 'bc28e06acb63a343e128aef02af1256bc00637ee70318ed24d0adde7cf3b598e7eacf2b8cba09c8ebf90b9a743e43a052dd1f6cf89f1e2d7487b1cd4a21ad5ba'
